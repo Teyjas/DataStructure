bfb5ba073714a94124299e881c258e9ff5cfd94b
Teyjas <97289921+Teyjas@users.noreply.github.com>
2022-03-05 23:04:17 +0530

    First_usecase

---
+++ DataStructure/DataStructure.cs
@@ -0,0 +1,15 @@
+﻿using MyDataStructureLibrary;
+
+Console.Title = "My Data Structure";
+Console.WriteLine("==========Custom Data Structure==========");
+
+UnOrderedLinkedList<int> list = new UnOrderedLinkedList<int>();
+
+Console.WriteLine("Adding 56, 30, 70 to list");
+list.Append(56);
+list.Append(30);
+list.Append(70);
+
+Console.WriteLine("List 1");
+list.Display();
+Console.ReadKey();
--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿namespace MyDataStructureLibrary;
+
+public class UnOrderedLinkedList<T> where T : IComparable<T>
+{
+    public Node<T> head;
+
+
+    public UnOrderedLinkedList()
+
+    {
+        head = null;
+    }
+    public UnOrderedLinkedList<T> List()
+    {
+        UnOrderedLinkedList<T> emptyList = new UnOrderedLinkedList<T>();
+        return emptyList;
+    }
+
+    public void Append(T data)
+    {
+        Node<T> node = new Node<T>(data);
+        if (head == null)
+            head = node;
+        else
+        {
+            Node<T> temp = head;
+            while (temp.next != null)
+                temp = temp.next;
+            temp.next = node;
+        }
+    }
+
+    public void Display()
+    {
+        Node<T> temp = head;
+        while (temp != null)
+        {
+            Console.Write("\n" + temp.data);
+            temp = temp.next;
+        }
+    }
+
+    
+}
